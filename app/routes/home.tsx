d5cb5eb7e26d627f6b4a8b25744447eb6b939a51
ricse <anthony.ricse98@gmail.com>
2026-08-19 02:49:23 -0500

    add background iamge

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/App";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Paradise" },
+    { name: "description", content: "Bienvenido a Paradise" },
   ];
 }
 
