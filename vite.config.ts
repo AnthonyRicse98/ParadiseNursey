379b9b0b92ee1579769d4c363334a07cfefde3b0
ricse <anthony.ricse98@gmail.com>
2026-08-19 02:10:36 -0500

    changes proyect

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
+  build: {
+    outDir: "dist", // Le indica a Vite que guarde la salida directamente en /dist
+  },
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
